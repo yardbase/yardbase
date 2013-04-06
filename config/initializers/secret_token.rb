91ae98f1298a6d02f91b915e0ef789e7eeaaca7b
Brian Samson <brian@briansamson.com>
2013-04-06 15:34:13 -0500

    test before deploying

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Yardbase::Application.config.secret_token = c3c9967d94df806fc1d9a0933fbf4243
+Yardbase::Application.config.secret_token = 'c3c9967d94df806fc1d9a0933fbf4243'
