b7c5680ff942a1bff4d5ed0943ceb8bef4d854b7
Bradley Grzesiak <brad@bendyworks.com>
2013-04-06 11:08:04 -0500

    rails_apps_composer: set up database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+# Environment variables (ENV['...']) are set in the file config/application.yml.
+# See http://railsapps.github.com/rails-environment-variables.html
+puts 'ROLES'
+YAML.load(ENV['ROLES']).each do |role|
+  Role.mongo_session['roles'].insert({ :name => role })
+  puts 'role: ' << role
+end
+puts 'DEFAULT USERS'
+user = User.create! :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
+puts 'user: ' << user.name
+user.add_role :admin
