65857ab1e2413fa7c206173763555db50baa3d7a
Bradley Grzesiak <brad@bendyworks.com>
2013-04-06 11:04:30 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yardbase::Application.config.secret_token = 'bdbafd7b50757f3c1be9f541076bf70e6f091bd5c9ba0fd3158551eb95e1e54b6b81e6d83fc069b185568b11e32a881bbb16a6428dc5a57df7b25bec30af870c'
